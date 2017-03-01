503964bba26aa06f80fd64699e174911d68ad3c3
mulrooneyju <mulrooneyju@uwplatt.edu>
2017-03-01 15:27:22 -0600

    Add files via upload
    Goes to added error

--- thirdhw.asm
+++ thirdhw.asm
@@ -3,11 +3,14 @@
 
 segment .data
 	msg1 db "Enter a postfix equation: "
+	test1 db "Added: "
 
 segment .bss
 	input resd 1
 	infix resd 1
 	prefix resd 1
+	operand resd 10 ;may be way too big or too small
+	count resd 1
 segment .text
         global  _asm_main
 
@@ -15,12 +18,27 @@ read_postfix:
 		mov eax, msg1
 		call print_string
 		call read_char
-		mov [input], eax
+		cmp eax, '+' 
+		je pushOp
+		cmp	eax, '-'
+		je pushOp
+		cmp eax, '*'
+		je pushOp
+		cmp eax, '/'
+		je pushOp
 		ret
 postfix_to_infix:
 
 
 postfix_to_prefix:
+
+pushOp:
+	;mov [opearnd], eax
+	mov eax, test1
+	call print_string
+	;mov eax, [operand]
+	call print_int
+	ret
 		
 _asm_main:
 	enter   0,0               ; setup routine
