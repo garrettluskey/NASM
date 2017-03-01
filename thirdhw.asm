0ccb4d12c437ce8aca6bde307f9787fad70e679d
garrettluskey <garrett.luskey@gmail.com>
2017-03-01 16:02:30 -0600

    Read_postfix now half works

--- thirdhw.asm
+++ thirdhw.asm
@@ -3,46 +3,34 @@
 
 segment .data
 	msg1 db "Enter a postfix equation: "
-	test1 db "Added: "
 
 segment .bss
-	input resd 1
+	input resd 10
 	infix resd 1
 	prefix resd 1
-	operand resd 10 ;may be way too big or too small
-	count resd 1
 segment .text
         global  _asm_main
 
 read_postfix:
 		mov eax, msg1
 		call print_string
+		do:
 		call read_char
-		cmp eax, '+' 
-		je pushOp
-		cmp	eax, '-'
-		je pushOp
-		cmp eax, '*'
-		je pushOp
-		cmp eax, '/'
-		je pushOp
+		call print_int
+		call print_nl
+		cmp al,10
+		jne do
+		return:
 		ret
 postfix_to_infix:
 
 
 postfix_to_prefix:
-
-pushOp:
-	;mov [opearnd], eax
-	mov eax, test1
-	call print_string
-	;mov eax, [operand]
-	call print_int
-	ret
 		
 _asm_main:
 	enter   0,0               ; setup routine
 	pusha
+	xor ecx,ecx
 	call read_postfix
 	
 			
