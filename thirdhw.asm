f886a9575857c50eee955796548d1f8099f40da8
garrettluskey <garrett.luskey@gmail.com>
2017-03-06 16:01:29 -0600

    Using stack

--- thirdhw.asm
+++ thirdhw.asm
@@ -6,10 +6,13 @@ segment .data
 	max_string_size equ 100
 segment .bss
 	input resd max_string_size
-	symbols resd max_string_size
+	operands resb max_string_size
+	buffer resd max_string_size
 	infix resd 0
 	prefix resd 1
 	count resd 0
+	operand resb 0
+	
 segment .text
 	global  _asm_main
 
@@ -18,6 +21,7 @@ read_postfix:
 	call print_string
 	xor ecx,ecx
 	xor edx, edx
+	xor ebx, ebx
 	do:
 	call read_char
 	mov [input + ecx], al
@@ -28,46 +32,61 @@ read_postfix:
 	ret
 		
 postfix_to_infix:
-		
+	dump_stack 1,2,3
 	xor ecx,ecx
 	xor edx, edx
-	doing:
+	xor ebx,ebx
+	first:
 	mov al,[input + ecx]
-	cmp al, '/'
-	je infix_middle
-	cmp al, '*'
-	je infix_middle
-	cmp al, '-'
-	je infix_middle
-	cmp al, '+'
-	je infix_middle
-	inc ecx
-	
 	cmp al, 10
-	call print_int
-	jne doing
-	;mov [infix + edx], 0xA
+	je return
+	cmp al, '/'
+	je infix_first
+	cmp al, '*'
+	je infix_first
+	cmp al, '-'
+	je infix_first
+	cmp al, '+'
+	je infix_first
+	push eax
+	dump_regs ecx
+	inc ecx
+	jmp first
+
+	return:
+	pop eax
+	mov [infix], eax
+	dump_regs 1
 	ret
+infix_first:
+	xor edx,edx
+	mov [buffer], edx
+	mov [buffer + 4], al
+	;call print_char
+	pop eax
+	;call print_int
+	mov [buffer], eax
+	pop eax
+	;call print_string
+	mov [buffer + 8], eax
+	mov eax,[buffer]
+	dump_regs 2
+	;call print_string
+	push eax
+	xor eax, eax
+	inc ecx
+	jmp first
 infix_middle:
-	mov al, [input + ecx - 2]
-	dump_regs edx
-	mov [infix + edx], eax
-	inc edx
-	mov al, [input + ecx]
-	mov [infix + edx], eax
-	inc edx
-	mov al, [input + ecx - 1]
-	mov [infix + edx], eax
-	inc edx
 postfix_to_prefix:
 		
 	mov [prefix + edx], eax
+	mov [count], ecx
+	sub ecx,ebx
 	inc edx
-	mov ebx, [input + ecx - 2]
-	mov [prefix + edx], ebx
-	inc edx
-	mov ebx, [input + ecx - 1]
-	mov [prefix + edx], ebx
+	mov al, [input + ecx - 1]
+	mov [prefix + edx], al
+	inc ebx
+	inc ebx
 	ret
 
 
@@ -79,6 +98,7 @@ print:
 _asm_main:
 	enter   0,0               ; setup routine
 	pusha
+	call print_int
 	call read_postfix
 	call postfix_to_infix
 	xor ecx,ecx
